c998da95cd98a0dafdf4bd73fb85ee2daa06b38e
Fabian Quist <frog_from_the_lake@gmx.de>
2024-04-02 14:01:44 +0200

    updated protos

--- IdentityProvider/pb/idp_svc_grpc.pb.go
+++ IdentityProvider/pb/idp_svc_grpc.pb.go
@@ -4,7 +4,7 @@ package pb
 
 import (
 	context "context"
-	login "github.com/Streamfair/streamfair_idp/pb/login"
+	login "github.com/Streamfair/common_proto/IdentityProvider/pb/login"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
