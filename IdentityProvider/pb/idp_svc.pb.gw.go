3aaa07839c16b2c05f90e2c87d025d9dec63708d
Fabian Quist <frog_from_the_lake@gmx.de>
2024-04-02 13:51:59 +0200

    updated protos

--- IdentityProvider/pb/idp_svc.pb.gw.go
+++ IdentityProvider/pb/idp_svc.pb.gw.go
@@ -13,7 +13,7 @@ import (
 	"io"
 	"net/http"
 
-	"github.com/Streamfair/common_proto/IdentityProvider/pb/login"
+	"github.com/Streamfair/streamfair_idp/pb/login"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/utilities"
 	"google.golang.org/grpc"
